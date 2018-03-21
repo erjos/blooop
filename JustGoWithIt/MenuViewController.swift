6e315766adf5bee123dbd4dffb3290f4cd015d71
Joseph <Ethan.Joseph@tdameritrade.com>
2018-03-21 14:36:58 -0400

    added place controller to add action on the menu

--- JustGoWithIt/MenuViewController.swift
+++ JustGoWithIt/MenuViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GooglePlaces
 
 class MenuViewController: UIViewController {
 
@@ -16,6 +17,12 @@ class MenuViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func add(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,16 +32,31 @@ class MenuViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MenuViewController: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        //Do work with place object
+        print(place.name)
+        print(place.formattedAddress)
+        print(place.attributions)
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        //Handle error
+        print(error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
 }
