04051741bf1952f287a4cc0f03da07006be0f58f
Joseph <Ethan.Joseph@tdameritrade.com>
2018-04-10 13:25:25 -0400

    launch google place picker from builder

--- JustGoWithIt/BuilderViewController.swift
+++ JustGoWithIt/BuilderViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GooglePlaces
 
 class BuilderViewController: UIViewController {
     //One idea would be to show and hide the fields as needed to reduce the noise on the page and only allow users to enter information in a predetermined order
@@ -15,10 +16,51 @@ class BuilderViewController: UIViewController {
         //hide date and name views at start
         nameView.isHidden = true
         dateView.isHidden = true
+        
+        //set field delegates
+        locationField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension BuilderViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if(locationField.isFirstResponder){
+            //launch google place picker
+            let autocompleteController = GMSAutocompleteViewController()
+            autocompleteController.delegate = self
+            present(autocompleteController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension BuilderViewController: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        //Do work with place object
+        print(place.name)
+        print(place.formattedAddress)
+        print(place.attributions)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        //Handle error
+        print(error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 }
