d51f885b61370c553005ed9d860a43e8ff4446e8
Joseph <Ethan.Joseph@tdameritrade.com>
2018-04-10 13:15:10 -0400

    some more view setup

--- JustGoWithIt/BuilderViewController.swift
+++ JustGoWithIt/BuilderViewController.swift
@@ -1,13 +1,20 @@
 import UIKit
 
 class BuilderViewController: UIViewController {
+    //One idea would be to show and hide the fields as needed to reduce the noise on the page and only allow users to enter information in a predetermined order
+    
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var dateView: UIView!
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var dateField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //hide date and name views at start
+        nameView.isHidden = true
+        dateView.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
