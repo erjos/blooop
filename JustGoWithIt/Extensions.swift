571eb222a4fbf23a828ee54b7707cd7cb6a4f851
Joseph <Ethan.Joseph@tdameritrade.com>
2018-06-15 11:54:27 -0400

    some more style changes to the builder

---
+++ JustGoWithIt/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  JustGoWithIt
+//
+//  Created by Joseph, Ethan on 6/15/18.
+//  Copyright © 2018 Joseph, Ethan. All rights reserved.
+//
+import Foundation
+import UIKit
+
+extension Date {
+    var day: Int { return Calendar.current.component(.day, from:self) }
+    var month: Int { return Calendar.current.component(.month, from:self) }
+    var year: Int { return Calendar.current.component(.year, from:self) }
+    
+    func formatDateAsString() -> String {
+        let dateFormater = DateFormatter()
+        if (self.month < 10) {
+            dateFormater.dateFormat = "M/dd/yy"
+        } else {
+            dateFormater.dateFormat = "MM/dd/yy"
+        }
+        return dateFormater.string(from: self)
+    }
+}
+
+extension UIView {
+    func dropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowRadius = 4.0
+        self.layer.cornerRadius = 5.0
+    }
+}
