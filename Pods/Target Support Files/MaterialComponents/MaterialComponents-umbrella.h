13b494ebcf3d64f6451b8401f94b88b64268515e
Joseph <Ethan.Joseph@tdameritrade.com>
2018-06-28 20:56:14 -0400

    pod install

--- Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
+++ Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -10,6 +10,12 @@
 #endif
 #endif
 
+#import "MaterialAppBar.h"
+#import "MDCAppBar.h"
+#import "MDCAppBarContainerViewController.h"
+#import "MaterialButtonBar.h"
+#import "MDCButtonBar.h"
+#import "MDCButtonBarButton.h"
 #import "MaterialButtons.h"
 #import "MDCButton.h"
 #import "MDCFlatButton.h"
@@ -20,10 +26,18 @@
 #import "MDCCard.h"
 #import "MDCCardCollectionCell.h"
 #import "UICollectionViewController+MDCCardReordering.h"
+#import "MaterialFlexibleHeader.h"
+#import "MDCFlexibleHeaderContainerViewController.h"
+#import "MDCFlexibleHeaderView.h"
+#import "MDCFlexibleHeaderViewController.h"
+#import "MaterialHeaderStackView.h"
+#import "MDCHeaderStackView.h"
 #import "MaterialInk.h"
 #import "MDCInkGestureRecognizer.h"
 #import "MDCInkTouchController.h"
 #import "MDCInkView.h"
+#import "MaterialNavigationBar.h"
+#import "MDCNavigationBar.h"
 #import "MaterialShadowElevations.h"
 #import "MDCShadowElevations.h"
 #import "MaterialShadowLayer.h"
@@ -38,6 +52,7 @@
 #import "MaterialIcons.h"
 #import "MDCIcons+BundleLoader.h"
 #import "MDCIcons.h"
+#import "MaterialIcons+ic_arrow_back.h"
 #import "MaterialIcons+ic_check_circle.h"
 #import "MaterialMath.h"
 #import "MDCMath.h"
@@ -49,6 +64,8 @@
 #import "MDCShapedShadowLayer.h"
 #import "MDCShapedView.h"
 #import "MDCShapeGenerating.h"
+#import "MaterialUIMetrics.h"
+#import "MDCLayoutMetrics.h"
 
 FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
 FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
