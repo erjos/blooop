9b8ba15b85094c33adb922654594a66be40df2d6
Ethan Joseph <ethanrj91@gmail.com>
2018-08-06 20:51:11 -0400

    removed unused button from trip mene

--- JustGoWithIt/Trip Details/TripViewController.swift
+++ JustGoWithIt/Trip Details/TripViewController.swift
@@ -129,7 +129,6 @@ class TripViewController: UIViewController {
             let destination = segue.destination as! PlaceModalViewController
             let indexPath = sender as! IndexPath
             destination.place = city.getSubLocation(from: indexPath)
-            // this is necessary to make sure the view Controller underneath doesn't get cleaned up
             destination.modalPresentationStyle = .overCurrentContext
         }
         if(segue.identifier == "presentMap"){
