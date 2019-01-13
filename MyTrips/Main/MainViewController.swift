0610a6cf1992297ad78d4b37312bdb87bb23f0e4
Ethan Joseph <ethanrj91@gmail.com>
2019-01-13 16:41:20 -0500

    started setting up location services

--- MyTrips/Main/MainViewController.swift
+++ MyTrips/Main/MainViewController.swift
@@ -11,28 +11,53 @@ import GoogleMaps
 
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var mapContainer: UIView!
+    var locationManager: CLLocationManager!
+    
+    var map: GMSMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-
-        // Do any additional setup after loading the view.
+        setupLocationManager()
+        locationManager.startUpdatingLocation()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupLocationManager(){
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        
     }
-    */
+    
+    private func setupMapView(target: CLLocationCoordinate2D){
+        //TODO: add method to load trips for when adding trips from storage
+        
+        var camera = GMSCameraPosition.camera(withTarget: target, zoom: 10)
+        map = GMSMapView.map(withFrame: mapContainer.bounds, camera: camera)
+        //map?.delegate = self
+        self.mapContainer.addSubview(map!)
+        
+        //map?.createMapMarkers(for: trip, map: map)
+    }
 }
 
 extension MainViewController: CLLocationManagerDelegate{
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //set map with the intial location
+        if let coordinate = locations.last?.coordinate {
+            setupMapView(target: coordinate)
+        }
+        //call this when running on simulator from view did load
+        else {
+            let coordinate = CLLocationCoordinate2D(latitude: 45.523450, longitude: -122.678897)
+            setupMapView(target: coordinate)
+        }
+        //stop updating location after that
+        //provide a refresh to start it up again
+    }
 }
