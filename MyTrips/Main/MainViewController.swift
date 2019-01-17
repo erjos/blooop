33018a9ee4b0961f9d620af60c655ff6e3a23102
Ethan Joseph <ethanrj91@gmail.com>
2019-01-17 18:31:41 -0500

    fixed up table view

--- MyTrips/Main/MainViewController.swift
+++ MyTrips/Main/MainViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 
 class MainViewController: UIViewController {
     @IBOutlet weak var drawerView: UIView!
     @IBOutlet weak var menuWidth: NSLayoutConstraint!
     @IBOutlet weak var mapContainer: GMSMapView!
+    @IBOutlet weak var placeTableView: UITableView!
     
     var locationManager: CLLocationManager!
     var map: GMSMapView?
@@ -25,6 +27,16 @@ class MainViewController: UIViewController {
             self.menuWidth.constant = (self.menuWidth.constant == 0) ? 300 : 0
             self.view.layoutIfNeeded()
         }
+    }
+    
+    @IBAction func tapSearch(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.autocompleteBoundsMode = .restrict
+        
+        //TODO: this should only be set if the user has already chosen a main location
+        //autocompleteController.autocompleteBounds = self.coordinateBounds
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -73,6 +85,9 @@ class MainViewController: UIViewController {
             locationManager.stopUpdatingLocation()
         }
         
+        ///TODO: we only want to set the coordinate bounds after the user chooses a central location
+        //coordinateBounds = LocationManager.getLocationBoundsFromMap(map: map!)
+        
         //map?.delegate = self
         //self.mapContainer.addSubview(map!)
         
@@ -80,10 +95,83 @@ class MainViewController: UIViewController {
     }
 }
 
-extension MainViewController: CLLocationManagerDelegate{
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = Bundle.main.loadNibNamed("PlaceTableHeaderView", owner: self, options: nil)?.first as?
+        UIView
+        return view
+    }
+}
+
+extension MainViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //set map with the intial location
-        setupMapView(coordinate: locations.last?.coordinate)        
+        setupMapView(coordinate: locations.last?.coordinate)
         //TODO: provide a location refresh mechanism on the page
+    }
+}
+
+extension MainViewController: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        //if(!isSubLocation){
+            //set city data using place data
+            //city.setCity(place: place)
+            GoogleResourceManager.sharedInstance.addGmsPlace(place: place)
+            
+            //TODO: test what happens when a user selects a city multiple times from the builder - does it change or update the model correctly?
+        //} else {
+            let location = SubLocation()
+            location.placeID = place.placeID
+            GoogleResourceManager.sharedInstance.addGmsPlace(place: place)
+            //append the new location to the end of the list at the appropriate index
+            //RealmManager.addSublocationsToCity(city: city, location: location)
+            //trip.cities[cityIndex].locations.append(location)
+        //}
+        //set the text field for location
+        //searchText.text = place.name
+        //searchText.textColor = UIColor.black
+        
+        //show the other fields
+        //locationDivider.isHidden = false
+        //nameView.isHidden = false
+        //dateView.isHidden = false
+        //doneButton.isHidden = false
+        
+        dismiss(animated: true, completion: nil)
+        UIView.animate(withDuration: 1.0 , animations: {
+            //
+            //self.nameViewHeight.constant = 120
+            //self.dateViewHeight.constant = 100
+            //TODO: set height of done button back to normal, if it is collapsed
+        }) { (complete) in
+            //
+        }
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        //Handle error
+        print(error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 }
